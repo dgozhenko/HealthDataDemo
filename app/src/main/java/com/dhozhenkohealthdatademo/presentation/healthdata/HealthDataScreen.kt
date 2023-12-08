dee100cdae82e638755008dd040852f363c4e195
dgozhenko <daniil.gozhenko@gmail.com>
2023-12-08 19:15:15 +0200

    Migrate from grid tile to row

--- app/src/main/java/com/dhozhenkohealthdatademo/presentation/healthdata/HealthDataScreen.kt
+++ app/src/main/java/com/dhozhenkohealthdatademo/presentation/healthdata/HealthDataScreen.kt
@@ -1,6 +1,9 @@
 package com.dhozhenkohealthdatademo.presentation.healthdata
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -16,6 +19,7 @@ import androidx.navigation.NavController
 import com.dhozhenkohealthdatademo.R
 import com.dhozhenkohealthdatademo.data.manager.HealthConnectManager
 import com.dhozhenkohealthdatademo.presentation.healthdata.component.HealthDataGridTile
+import com.dhozhenkohealthdatademo.presentation.healthdata.component.HealthDataRow
 import com.dhozhenkohealthdatademo.util.formatDoubleToString
 
 @ExperimentalMaterial3Api
@@ -31,53 +35,51 @@ fun HealthDataScreen(
     Scaffold(
         modifier = Modifier.fillMaxSize()
     ) { scaffoldPadding ->
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = 120.dp),
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(scaffoldPadding)
                 .padding(horizontal = 24.dp)
         ) {
-            item {
-                val lastData = state.stepsData.steps.maxByOrNull { it.date }
-                HealthDataGridTile(
-                    title = "My Steps",
-                    value = if (lastData?.count != null) formatDoubleToString(lastData.count.toDouble()) else null,
-                    units = "",
-                    icon = R.drawable.step_icon,
-                    loading = state.stepsData.loading
-                )
-            }
-            item {
-                val lastData = state.distanceData.distances.maxByOrNull { it.date }
-                HealthDataGridTile(
-                    title = "Distance",
-                    value = if (lastData?.value != null) formatDoubleToString(lastData.value.toDouble()) else null,
-                    units = if (lastData?.value != null) "km" else "",
-                    icon = R.drawable.ruler_icon,
-                    loading = state.distanceData.loading
-                )
-            }
-            item {
-                val lastData = state.caloriesData.calories.maxByOrNull { it.date }
-                HealthDataGridTile(
-                    title = "Calories",
-                    value = if (lastData?.value != null) formatDoubleToString(lastData.value.toDouble()) else null,
-                    units = if (lastData?.value != null) "kcal" else "",
-                    icon = R.drawable.fire_icon,
-                    loading = state.caloriesData.loading
-                )
-            }
-            item {
-                val lastData = state.sleepData.sleepDatas.maxByOrNull { it.date }
-                HealthDataGridTile(
-                    title = "Bed Time",
-                    value = lastData?.hours,
-                    units = "",
-                    icon = R.drawable.sleep_icon,
-                    loading = state.stepsData.loading
-                )
-            }
+            val lastStepsData = state.stepsData.steps.maxByOrNull { it.date }
+            val lastDistanceData = state.distanceData.distances.maxByOrNull { it.date }
+            val lastCaloriesData = state.caloriesData.calories.maxByOrNull { it.date }
+            val lastSleepData = state.sleepData.sleepDatas.maxByOrNull { it.date }
+
+            HealthDataRow(
+                title = "My Steps",
+                value = if (lastStepsData?.count != null) formatDoubleToString(lastStepsData.count.toDouble()) else null,
+                units = "",
+                icon = R.drawable.step_icon,
+                loading = state.stepsData.loading
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            HealthDataRow(
+                title = "Distance",
+                value = if (lastDistanceData?.value != null) formatDoubleToString(lastDistanceData.value.toDouble()) else null,
+                units = if (lastDistanceData?.value != null) "km" else "",
+                icon = R.drawable.ruler_icon,
+                loading = state.distanceData.loading
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            HealthDataRow(
+                title = "Calories",
+                value = if (lastCaloriesData?.value != null) formatDoubleToString(lastCaloriesData.value.toDouble()) else null,
+                units = if (lastCaloriesData?.value != null) "kcal" else "",
+                icon = R.drawable.fire_icon,
+                loading = state.caloriesData.loading
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            HealthDataRow(
+                title = "Bed Time",
+                value = lastSleepData?.hours,
+                units = "",
+                icon = R.drawable.sleep_icon,
+                loading = state.stepsData.loading
+            )
         }
     }
 }
--- app/src/main/java/com/dhozhenkohealthdatademo/presentation/healthdata/component/HealthDataGridTile.kt
+++ app/src/main/java/com/dhozhenkohealthdatademo/presentation/healthdata/component/HealthDataGridTile.kt
@@ -1,24 +1,76 @@
 package com.dhozhenkohealthdatademo.presentation.healthdata.component
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.dhozhenkohealthdatademo.util.formatDoubleToString
+
+@ExperimentalMaterial3Api
+@Composable
+fun HealthDataRow(
+    title: String,
+    value: String?,
+    units: String?,
+    loading: Boolean,
+    @DrawableRes icon: Int,
+) {
+    Card(onClick = { }, modifier = Modifier.fillMaxWidth()) {
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp)
+        ) {
+            Spacer(modifier = Modifier.width(8.dp))
+            IconWithProgressIndicator(icon = icon, title = title, isLoading = loading)
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(text = title, style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
+                if (value != null) {
+                    Row {
+                        Text(text = value, style = TextStyle(fontSize = 16.sp))
+                        Text(text = " $units" ?: "")
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(
+                imageVector = Icons.Default.ArrowForward,
+                contentDescription = "Arrow forward Icon",
+                modifier = Modifier
+                    .border(width = 1.dp, color = Color.White, shape = CircleShape)
+                    .padding(8.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+    }
+}
 
 @ExperimentalMaterial3Api
 @Composable
