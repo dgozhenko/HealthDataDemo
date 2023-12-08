e0c82b9f32439bf04fc155b1df8cb885bb734633
dgozhenko <daniil.gozhenko@gmail.com>
2023-12-08 19:16:31 +0200

    Add top bar

--- app/src/main/java/com/dhozhenkohealthdatademo/presentation/healthdata/HealthDataScreen.kt
+++ app/src/main/java/com/dhozhenkohealthdatademo/presentation/healthdata/HealthDataScreen.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -33,6 +35,9 @@ fun HealthDataScreen(
     val state by viewModel.healthDataState.collectAsState()
 
     Scaffold(
+        topBar = {
+                 CenterAlignedTopAppBar(title = { Text(text = "Health & Activity Insights") })
+        },
         modifier = Modifier.fillMaxSize()
     ) { scaffoldPadding ->
         Column(
