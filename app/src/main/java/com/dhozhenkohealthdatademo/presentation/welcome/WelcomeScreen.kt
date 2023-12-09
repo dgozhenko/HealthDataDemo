dcf51ff3ca5e8611504cc86710e7066637211c28
dgozhenko <daniil.gozhenko@gmail.com>
2023-12-09 13:23:47 +0200

    Update welcome screen

--- app/src/main/java/com/dhozhenkohealthdatademo/presentation/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/dhozhenkohealthdatademo/presentation/welcome/WelcomeScreen.kt
@@ -1,6 +1,7 @@
 package com.dhozhenkohealthdatademo.presentation.welcome
 
 import android.content.Intent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,11 +10,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,6 +44,7 @@ import com.dhozhenkohealthdatademo.HEALTH_CONNECT_SETTINGS_ACTION
 import com.dhozhenkohealthdatademo.R
 import com.dhozhenkohealthdatademo.data.manager.HealthConnectManager
 import com.dhozhenkohealthdatademo.domain.navigation.NavigationRoute
+import com.dhozhenkohealthdatademo.ui.theme.MainColor
 import com.dhozhenkohealthdatademo.util.OnLifecycleEvent
 import kotlinx.coroutines.launch
 
@@ -46,6 +55,9 @@ fun WelcomeScreen(
 
     val activity = LocalContext.current
     val scope = rememberCoroutineScope()
+    var isShowMoreVisible by remember {
+        mutableStateOf(false)
+    }
 
     OnLifecycleEvent { _, event ->
         when (event) {
@@ -69,7 +81,7 @@ fun WelcomeScreen(
 
     Scaffold(modifier = Modifier.fillMaxSize()) { scaffoldPadding ->
         Column(
-            verticalArrangement = Arrangement.Top,
+            verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .fillMaxSize()
@@ -84,20 +96,35 @@ fun WelcomeScreen(
             Spacer(modifier = Modifier.height(24.dp))
             Text(
                 text = stringResource(R.string.welcome_title), style = TextStyle(
-                    fontSize = 24.sp, fontWeight = FontWeight.Bold, color = Color.Blue
+                    fontSize = 24.sp, fontWeight = FontWeight.Bold, color = MainColor
                 )
             )
             Spacer(modifier = Modifier.height(12.dp))
-            Text(
-                text = stringResource(R.string.welcome_detail), style = TextStyle(fontSize = 16.sp)
-            )
-            Spacer(modifier = Modifier.height(12.dp))
-            Text(
-                text = stringResource(R.string.welcome_features_list),
-                style = TextStyle(fontSize = 16.sp),
-                modifier = Modifier.fillMaxWidth()
-            )
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MainColor
+                ), modifier = Modifier
 
+            ) {
+                Text(
+                    text = stringResource(R.string.welcome_detail),
+                    style = TextStyle(fontSize = 16.sp, color = Color.White),
+                    modifier = Modifier.padding(12.dp)
+                )
+                TextButton(onClick = { isShowMoreVisible = !isShowMoreVisible }) {
+                    Text(text = if (isShowMoreVisible) "Show less" else "Show more", style = TextStyle(fontWeight = FontWeight.Bold, color = Color.White))
+                }
+
+                AnimatedVisibility(visible = isShowMoreVisible) {
+                    Column {
+                        Text(
+                            text = stringResource(R.string.welcome_features_list),
+                            style = TextStyle(fontSize = 16.sp, color = Color.White),
+                            modifier = Modifier.fillMaxWidth().padding(12.dp)
+                        )
+                    }
+                }
+            }
             Spacer(modifier = Modifier.weight(1f))
 
             Spacer(modifier = Modifier.height(12.dp))
@@ -107,7 +134,7 @@ fun WelcomeScreen(
                 settingsIntent.action = HEALTH_CONNECT_SETTINGS_ACTION
                 activity.startActivity(settingsIntent)
             }, modifier = Modifier.fillMaxWidth()) {
-                Text(text = "Enable access to your health data now")
+                Text(text = "Enable access to your health data")
             }
             Spacer(modifier = Modifier.height(36.dp))
         }
